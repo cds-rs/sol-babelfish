fe362eb46988ca42ac00709f360cbeaf204b409d
cds-amal <cds.sudama@gmail.com>
2026-07-03 10:18:25 -0400

    Rebuild previews with the ship-of-fools intro

--- docs/subscriptions/searcher-09f2665d.js
+++ docs/subscriptions/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-116b68c0.js',
+                path_to_root + 'searchindex-938aa96d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
