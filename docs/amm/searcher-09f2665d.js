c106ad76c4b9a38e07551518cdc493b8f3e00cdc
cds-amal <cds.sudama@gmail.com>
2026-07-03 11:12:06 -0400

    Rebuild previews (AMM source lines follow the formatted tests)

--- docs/amm/searcher-09f2665d.js
+++ docs/amm/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b0f996dc.js',
+                path_to_root + 'searchindex-d34beeda.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- docs/record/searcher-09f2665d.js
+++ docs/record/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-114a6eec.js',
+                path_to_root + 'searchindex-aa96a2fb.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
